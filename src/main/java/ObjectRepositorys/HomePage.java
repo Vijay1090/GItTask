47d4e43b316ad7fb7e916edc939c8c18b1f39d10
VijayDYaligar <vijayy8981@gmail.com>
2024-03-15 20:37:27 +0530

    HomePageFunctionality and Login Functionlity

---
+++ src/main/java/ObjectRepositorys/HomePage.java
@@ -0,0 +1,18 @@
+package ObjectRepositorys;
+
+public class HomePage {
+	public void getLogo() {
+		System.out.println("logo displayed");
+	}
+	public void clickonMenuLink() {
+		System.out.println("clickd on menu link");
+	}
+	public void clickonUserprofileLink() {
+		System.out.println("redirected to userprofile");
+		
+	}
+	public void clickoncartLink() {
+		System.out.println("redirected to cart page");
+	}
+
+}
