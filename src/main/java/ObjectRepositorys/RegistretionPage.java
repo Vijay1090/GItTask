fcc77e64a9ebbdb467f9aaabe1b0a91fceeb3131
Vijay <vijay.yaligar77@gmail.com>
2024-03-15 12:50:43 +0530

    Registration_Functionality

---
+++ src/main/java/ObjectRepositorys/RegistretionPage.java
@@ -0,0 +1,16 @@
+package ObjectRepositorys;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+
+public class RegistretionPage {
+	public void TC() {
+		System.out.println("I have added Registetion page");
+	}
+	
+
+}
---
+++ src/test/java/com/tests/RegistrationTest.java
@@ -0,0 +1,14 @@
+package com.tests;
+
+public class RegistrationTest {
+	public void testCase1() {
+		System.out.println("Verified Registration logo");
+	}
+   
+	public void testCase2() {
+		System.out.println("Verified all registration element");
+	}
+	public void testCase3() {
+		System.out.println("Verified Registration Functionality");
+	}
+}
